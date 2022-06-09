3143389c398407dd1f77e9d6624a50f5542bf353
Akilah Snowden <akilahdsnowden@gmail.com>
2022-06-09 18:38:42 -0400

    commit tree e040355e56063899b42f4ac03820e44d78998b26

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
+# takes in an argument of a dog's name and assigns that value to a variable,
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+# reporting the name
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+ puts lassie.name
